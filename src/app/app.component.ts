ee5ceb8aa21222164ef0cade6476243cd679e2b8
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-11-05 18:47:32 +0600

    navigation between recipes and shopping list button using custom event binding from header to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecipeApp';
+  navigationTo = 'recipes';
+
+  onNavigationClicked(navigationToValue: {navigatedValue: string}) {
+    this.navigationTo = navigationToValue.navigatedValue;
+    console.log(this.navigationTo);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output()
+  navigateToEvent = new EventEmitter<{navigatedValue: string}>();
+
+  onClickRecipes() {
+    this.navigateToEvent.emit({navigatedValue: 'recipes'});
+  }
+
+  onClickShoppingList() {
+    this.navigateToEvent.emit({navigatedValue: 'shoppingList'});
+  }
 
 }

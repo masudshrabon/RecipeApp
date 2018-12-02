863e6bde9f0ff2894b9199aef29b8e7c108797f2
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-02 18:09:10 +0600

    implement 'to shopping list' button using shopping list service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,16 @@ export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  addIngredientsToShoppingList() {
+    console.log(this.recipe.ingredients);
+    this.recipe.ingredients.forEach(ingredient => {
+      this.shoppingListService.addIngredient(ingredient);
+    });
   }
 
 }

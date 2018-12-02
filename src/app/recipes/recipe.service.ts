336779ca61cf9909fdb8cfb338e44b40b95389e7
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-02 16:51:28 +0600

    add ingredients to recipe model for recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,28 @@
 
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'src/assets/images/testRecipe.png'),
-    new Recipe('A Test Recipe2', 'This is simply another test', 'src/assets/images/testRecipe.png')
+    new Recipe('Test Recipe',
+      'The is a test recipe.',
+      'src/assets/images/testRecipe.png',
+      [new Ingredient('Test Ingredient1', 1), new Ingredient('Test Ingredient2', 2)]
+    ),
+    new Recipe('Beef Pasta',
+      'The recipe is tasty.',
+      'src/assets/images/pasta.png',
+      [new Ingredient('Beef', 1), new Ingredient('Pasta', 1), new Ingredient('Tomato', 2)]
+    ),
+    new Recipe('Vegetable Noodles',
+      'The recipe is awesome.',
+      'src/assets/images/vegetableNoodles.png',
+      [new Ingredient('Noodles', 1), new Ingredient('Tomato', 2), new Ingredient('Potato', 2), new Ingredient('Carrot', 2)]
+    )
   ];
 
   getRecipes() {

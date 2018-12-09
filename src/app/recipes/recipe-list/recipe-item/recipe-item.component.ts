d7ea9a7f1047853d91bc11bd29d8257a160893cb
Masudur Rahman <masudur.rahman@kaz.com.bd>
2018-12-09 15:47:03 +0600

    added active class to recipe item and refactor getRicipe in the component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,12 +11,13 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  recipe: Recipe;
   @Input() index: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.recipe = this.recipeService.getRecipe(this.index);
   }
 
 }

98625f6cdfa205a3ce563c64214ac4ae381896f0
masudshrabon <shrabon_bit0413@yahoo.com>
2019-01-05 19:53:38 +0600

    edit recipe part 2

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
@@ -34,18 +34,30 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    const recipeIngredients = new FormArray([]);
+
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
-    }
+      if (recipe['ingredients']) {
+        for (const ingredient of recipe.ingredients) {
+          console.log(ingredient.amount);
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          }));
+        }
+      }
 
-    this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
-    });
+      this.recipeForm = new FormGroup({
+        'name': new FormControl(recipeName),
+        'imagePath': new FormControl(recipeImagePath),
+        'description': new FormControl(recipeDescription),
+        'ingredients': recipeIngredients
+      });
+    }
   }
 
 }

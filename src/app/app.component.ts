f8b9e99c7e04f2ee2a1c172cea2643a078b3844e
Masudur Rahman <masudur.rahman@kaz.com.bd>
2019-01-23 13:54:10 +0600

    install and setting up firebase sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Recipe App';
 
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDf1ShCrMxHdxDgmmY45RP8ps1DV55Bl8s',
+      authDomain: 'ng-recipe-app-project-id.firebaseapp.com'
+    });
+  }
 }

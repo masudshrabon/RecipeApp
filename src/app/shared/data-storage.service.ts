60db8039ac2f63e00d983d32e156721e140105a0
Masudur Rahman <masudur.rahman@kaz.com.bd>
2019-01-30 13:01:28 +0600

    get token id using firebase and used it in recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -5,22 +5,28 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
   constructor(
     private http: Http,
     private recipeService: RecipeService,
-    private shoppingListService: ShoppingListService
+    private shoppingListService: ShoppingListService,
+    private authService: AuthService
   ) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-app-project-id.firebaseio.com/recipes.json',
-    this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-recipe-app-project-id.firebaseio.com/recipes.json?auth=' + token,
+      this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-app-project-id.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get('https://ng-recipe-app-project-id.firebaseio.com/recipes.json?auth=' + token)
       .pipe(map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();

9461cb095eee8ba22c611dcbfa43193ca31f22d0
Masudur Rahman <masudur.rahman@kaz.com.bd>
2019-01-30 18:38:33 +0600

    added auth guard to edit and create recipe

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -12,6 +18,7 @@ export class AuthService {
       .then(
         response => {
           console.log(response);
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken().then(
             (token: string) => this.token = token
           );
